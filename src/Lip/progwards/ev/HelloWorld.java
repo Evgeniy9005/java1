a13daf4e4e6e4bd566f623b0064cd00517149fa0
user <Evgeniy_90_05@mail.ru>
2019-11-04 16:38:47 +0300

    Revert "addition1"
    This reverts commit 1126d7b5

--- src/Lip/progwards/ev/HelloWorld.java
+++ src/Lip/progwards/ev/HelloWorld.java
@@ -1,15 +1,8 @@
 package Lip.progwards.ev;
 
 public class HelloWorld {
-    static int addition(int x, int y){
-        return x+y;
-    }
-    static int addition1(int x, int y){
-        return x+y;
-    }
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println(addition(5,8));
     }
 
 }
