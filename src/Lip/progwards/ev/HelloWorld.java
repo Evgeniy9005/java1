815c06ab6a4818c6fa74b735714030bc5912dd4d
user <Evgeniy_90_05@mail.ru>
2019-11-07 16:48:06 +0300

    ru.progwards.java1.lessons.basics

--- src/Lip/progwards/ev/HelloWorld.java
+++ src/Lip/progwards/ev/HelloWorld.java
@@ -4,8 +4,21 @@ public class HelloWorld {
     static int addition(int x, int y){
         return x+y;
     }
+    static double fractional(double num){
+        double result1=num+1.0;
+        double result=result1 / result1;
+        return result1 % result;
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World! \"Привет Мир\"");
+        float pi=3.1415926535f;
+        double koe =15;
+        double d1 = 5 / 2;
+        double d2 = 5.234 % 2;
+        String  message = "Запись фильма \"Матрица\" находится в файле C:\\video\\matrix.avi";
+        System.out.println(d2);
+        System.out.println(fractional(1.5));
     }
 
 }
