3925c80470e1db35f5aefeac63425bf159d1a622
user <Evgeniy_90_05@mail.ru>
2019-11-13 22:05:16 +0300

    ru.progwards.java1.lessons.compare_if_cycles

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -23,30 +23,27 @@ public class CyclesGoldenFibo {
     // Он должен быть равнобедренным и отношение ребра к основанию должно лежать между значениями 1.61703 и 1.61903.
     public static boolean isGoldenTriangle(int a, int b, int c){
 
-        double besis=0d;
+        double besis=0.0;
         boolean result=false;
         if (a==b || a==c ||b==c ) {
-            if (a==b) {besis= Math.sqrt(a*a-(c/2)*(c/2));
-            besis=besis/a;
-            }
+            if (a==b) besis=(double)a/c;
             // Выражение вычисляет длину основания равнобедренного треугольник при равных сторонах a=b
-            if (b==c) {besis= Math.sqrt(b*b-(a/2)*(a/2));
-            besis=besis/b;
-            }
+            if (b==c) besis=(double)b/a;
             // Выражение вычисляет длину основания равнобедренного треугольник при равных сторонах b=c
-            if (c==a) {besis= Math.sqrt(c*c-(b/2)*(b/2));
-          //  besis=a/besis;
-            besis=besis/a;
-            }
+            if (c==a) besis=(double)c/b;
             // Выражение вычисляет длину основания равнобедренного треугольник при равных сторонах c=a
-            if (besis>=1.61703 && besis<=1.61903)  result=true;
-            System.out.println(besis);
+            if (besis>=1.61703 && besis<=+1.61903)  result=true;
+
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(fiboNumber(10));
-        System.out.println(isGoldenTriangle(18,9,18));
+        // Выводит на экран числа Фибоначи от 1 до 15
+        for (int i=0;i<=15;i++ )System.out.println(fiboNumber(i));
+        // Выводит на экран значение сторон Золотых треугольников в диапазоне натуральных чисел от 1 до 100
+        for (int i1=0;i1<=100;i1++) {for (int i=0;i<=100;i++)
+            if (isGoldenTriangle(i,i1,i) == true)
+                System.out.println("Золотой треугольник a="+i+" b="+i1+" c="+i);}
     }
 }
