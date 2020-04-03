bc572301b3450ef1b694e103c4323776898122a4
user <Evgeniy_90_05@mail.ru>
2020-04-03 17:41:48 +0300

    ru.progwards.java1.lessons.interfaces

---
+++ src/Test/Test.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int t=0;
+        System.out.println("Напишите ответ от одного до четырех");
+        System.out.write(t);
+    }
+}
